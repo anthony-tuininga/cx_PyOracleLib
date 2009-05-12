1cad2143b13aad92815a27a86dedc4dc95940bee
Anthony Tuininga <anthony-tuininga@users.noreply.github.com>
2009-05-12 19:26:03 +0000

    cx_SQL has been replaced by a combination of cx_PatchCommands and cx_OracleParser.SimpleParser. cx_CursorCache is not used and is simple enough to recode if needed.

---
+++ cx_PatchCommands.py
@@ -0,0 +1,87 @@
+"""Module for use in patching databases by a number of means."""
+
+import cx_Logging
+import cx_OracleEx
+import cx_OracleParser
+import datetime
+import os
+import sys
+
+class Processor(object):
+
+    def __init__(self, connection):
+        self.connection = connection
+
+    def _LogCommand(self, command):
+        separator = "-" * 66
+        message = command.GetLogMessage()
+        cx_Logging.Trace("%s", separator)
+        cx_Logging.Trace("%s", message)
+        cx_Logging.Trace("%s", separator)
+        if sys.stdout.isatty() and not sys.stderr.isatty():
+            now = datetime.datetime.today()
+            print now.strftime("%Y/%m/%d %H:%M:%S"), message
+            sys.stdout.flush()
+
+    def ProcessCommand(self, command):
+        self._LogCommand(command)
+        command.Process(self.connection)
+
+    def ProcessFile(self, fileName):
+        name, extension = os.path.splitext(fileName)
+        if not extension:
+            extension = ".sql"
+            fileName += extension
+        cls = CommandBase.classByExtension.get(extension and extension.lower())
+        if cls is None:
+            cls = ExecuteSQLCommands
+        command = cls(fileName)
+        self.ProcessCommand(command)
+
+
+class CommandMetaClass(type):
+
+    def __init__(cls, name, bases, classDict):
+        super(CommandMetaClass, cls).__init__(name, bases, classDict)
+        if cls.extension is not None:
+            cls.classByExtension[cls.extension] = cls
+
+
+class CommandBase(object):
+    __metaclass__ = CommandMetaClass
+    classByExtension = {}
+    extension = None
+
+    def __init__(self, fileName):
+        self.fileName = fileName
+
+    def GetLogMessage(self):
+        return "Executing commands in %s." % self.fileName
+
+
+class ExecuteSQLCommands(CommandBase):
+    extension = ".sql"
+
+    def Process(self, connection):
+        cursor = connection.cursor()
+        cursor.execute("select user from dual")
+        user, = cursor.fetchone()
+        parser = cx_OracleParser.SimpleParser()
+        sql = open(self.fileName).read().strip()
+        connectStatementClass = parser.parser.processor.ConnectStatement
+        try:
+            for statement in parser.Parse(sql, user, productionName = "file"):
+                if isinstance(statement, connectStatementClass):
+                    connection = cx_OracleEx.Connection(statement.user,
+                            statement.password or connection.password,
+                            statement.dsn or connection.dsn)
+                    cursor = connection.cursor()
+                    cx_Logging.Trace("%s", statement.GetLogMessage(cursor))
+                    parser.parser.processor.owner = statement.user
+                else:
+                    statement.Process(cursor)
+        except cx_OracleParser.ParsingFailed, value:
+            cx_Logging.Error("Parsing failed at position %s (%s...)",
+                    value.arguments["pos"],
+                    value.arguments["remainingString"][:100])
+
--- setup.py
+++ setup.py
@@ -7,39 +7,34 @@ To install:
 
 from distutils.core import setup
 
-# NOTE: distutils does not currently support specifying both modules and
-# packages so the submodules are specified independently; this can be
-# eliminated in Python 2.3 and up
 modules = [
-        "cx_CursorCache",
         "cx_ExportData",
         "cx_ImportData",
         "cx_OracleDebugger",
         "cx_OracleEx",
-        "cx_OracleObject.Describer",
-        "cx_OracleObject.Environment",
-        "cx_OracleObject.Object",
-        "cx_OracleObject.Statements",
-        "cx_OracleObject.Utils",
-        "cx_OracleParser.Dependency",
-        "cx_OracleParser.Grammar",
-        "cx_OracleParser.Parser",
-        "cx_OracleParser.Statements",
         "cx_OracleUtils",
-        "cx_SQL"
+        "cx_PatchCommands"
+]
+
+packages = [
+        "cx_OracleObject",
+        "cx_OracleParser",
+        "cx_OracleParser.full",
+        "cx_OracleParser.simple"
 ]
 
 
 setup(
         name = "cx_PyOracleLib",
-        version = "HEAD",
+        version = "2.5",
         description = "Set of Python modules for handling Oracle databases",
-        licence = "See LICENSE.txt",
+        license = "See LICENSE.txt",
         long_description = "Set of Python modules for handling Oracle " + \
                 "databases and used by a number of Computronix projects, " + \
                 "in particular the cx_OracleTools project",
         author = "Anthony Tuininga",
         author_email = "anthony.tuininga@gmail.com",
         url = "http://starship.python.net/crew/atuining",
-        py_modules = modules)
+        py_modules = modules,
+        packages = packages)
 
